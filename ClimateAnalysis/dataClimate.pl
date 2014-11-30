a89495fc68e5a26e3989b779ab32a6e267e46432
Siqi Liu <flycat1989@gmail.com>
2014-11-30 15:33:14 -0500

    added script for climate data acquisition

--- ClimateAnalysis/dataClimate.pl
+++ ClimateAnalysis/dataClimate.pl
@@ -3,23 +3,22 @@ use HTML::TableExtract;
 
 
 sub getHTML {
-$url_id=@_[0];
-$doc_id=@_[1];
-$state_id=@_[2];
-
-$group1_id=$doc_id.".V9-level2";
-$group2_id=$doc_id.".V1";
-$field_id="F_".$doc_id.".V9";
+$state_id=@_[0];
+$year_id=@_[1];
 
 my $mech = WWW::Mechanize->new();
-$url="http://wonder.cdc.gov/".$url_id.".html";
+$url="http://wonder.cdc.gov/nasa-nldas.html";
 
 $mech->get( $url );
-$mech->click_button( name => "action-I Agree");
+$mech->tick("M_3"=>"D60a.M3");
+$mech->set_fields("B_1"=>"D60a.V2-level2",
+		"B_2"=>"D60a.V4",
+		
+		"F_D60a.V2"=>$state_id,
 
-$mech->set_fields("B_1"=>$group1_id,
-		"B_2"=>$group2_id,
-		$field_id=>$state_id,
+
+		"F_D60a.V7"=>$year_id,
+		
 	);
 
 $mech->click_button( name=>"action-Send");
@@ -28,7 +27,7 @@ $htmlResult=$mech->content();
 $te=new HTML::TableExtract(depth=>1, count=>4);
 $te->parse($htmlResult);
 
-open (WRITE_TABLE, ">", "MortalityData/$url_id-$state_id.txt");
+open (WRITE_TABLE, ">", "ClimateData/$state_id-$year_id.txt");
 foreach $row ($te->rows) {
    print WRITE_TABLE join("\t", @$row), "\n";
 }
@@ -40,22 +39,26 @@ close(WRITE_TABLE);
 
 }
 
-@url_list=("cmf-icd8","cmf-icd9","cmf-icd10");
-@doc_list=("D74","D16","D103");
+@year_list=("1979","1980","1981","1982","1983","1984",
+"1985","1986","1987","1988","1989","1990",
+"1991","1992","1993","1994","1995","1996",
+"1997","1998","1999","2000","2001","2002",
+"2003","2004","2005","2006","2007","2008",
+"2009","2010","2011");
 
 
-@state_list=("01","02"    ,"04","05","06"    ,"08","09","10","11","12","13"    ,"15",
+@state_list=("01"    ,"04","05","06"    ,"08","09","10","11","12","13"    ,"15",
 		"16","17","18","19","20","21","22","23","24","25","26","27","28","29","30",
 		"31","32","33","34","35","36","37","38","39","40","41","42"    ,"44","45",
 		"46","47","48","49","50","51"   ,"53","54","55","56");
-for $url_doc_ptr (0..2){
-	foreach $state_ptr (@state_list){
-		print "Now processing $url_list[$url_doc_ptr]-$state_ptr ...\n";		
-		$filename = "MortalityData/$url_list[$url_doc_ptr]-$state_ptr.txt";
+foreach $state_ptr (@state_list){
+	foreach $year_ptr (@year_list){
+		print "Now processing $state_ptr-$year_ptr ...\n";		
+		$filename = "ClimateData/$state_ptr-$year_ptr.txt";
  		if (-e $filename) {
- 				print "File Exists! Moving to the next file\n";
+ 			print "File Exists! Moving to the next file\n";
 		 }else{ 
-			getHTML($url_list[$url_doc_ptr],$doc_list[$url_doc_ptr],$state_ptr);
+			getHTML($state_ptr,$year_ptr);
 		}
 	}
 }
